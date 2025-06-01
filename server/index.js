e9f7f9bf6e87dbdf1999ce3b43eaa9aef58b3917
rymxndwg <mechaiboi@gmail.com>
2025-06-01 01:56:18 -0400

    Added Backend

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import complaintRoutes from './routes/complaintSubmit.js'
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/', complaintRoutes); // Mount the complaint routes
+
+// ✅ Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.error('❌ MongoDB error:', err));
+
+
+// ======================
+// 🚀 START SERVER
+// ======================
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
