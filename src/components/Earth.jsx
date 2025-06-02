5e814db90adaf5c4b11bbcdb86dc38b284b85696
rymxndwg <mechaiboi@gmail.com>
2025-06-02 19:48:27 -0400

    Earth Video

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -1,8 +1,9 @@
 import '../css/Earth.css'
 
 function Earth(){
-    return <div id = "earth"> 
-      </div>
+    return <video width="640" height="360" controls="autoPlay">
+      <source src="/videos/SpinningEarth.mp4" type="video/mp4" />
+    </video>
 }
 
 export default Earth
