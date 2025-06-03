145fbc4aa170a129aa841a80ea62bf4d81edbd1c
rymxndwg <mechaiboi@gmail.com>
2025-06-03 13:56:20 -0400

    Fixed Db Settings 5001 Port

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/complaintSubmit': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5001',
         changeOrigin: true,
       },
     },
