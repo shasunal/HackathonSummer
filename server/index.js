7b132ecd5ee6696ebe8dd2fc1c556d24160ffbce
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2025-06-12 15:47:09 -0400

    added port 5001

--- server/index.js
+++ server/index.js
@@ -15,5 +15,5 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB error:', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
