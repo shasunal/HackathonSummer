89a01e73020a621ad6a71ec9df50b370fa37f41a
rymxndwg <mechaiboi@gmail.com>
2025-06-12 21:55:37 -0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import ComplaintForm from '../components/ComplaintForm';
 import Earth from '../components/Earth';
+import { useLocation } from 'react-router-dom';
 import '../css/Home.css'
 
 function Home(){
+    const location = useLocation();
+    const missingInfo = location.state?.missingInfo;
+
     return <div className = "home-container">
-        <ComplaintForm />
-        <Earth />
+        {!missingInfo ? (  //HTML for if first time loading page
+            <>
+                <ComplaintForm missingInfo = {missingInfo}/>
+                <Earth />
+            </>
+        ) : (  //HTML for if missing info is required
+            <>
+               <ComplaintForm missingInfo = {missingInfo}/>
+            </>
+        )}
+
+        {!missingInfo ? (<></>) : (<></>)
+        }
+
     </div>
-    
 }
 
 export default Home
