a73e953bad87f78a2460a64ea4186629df2e390d
rymxndwg <mechaiboi@gmail.com>
2025-06-16 13:33:34 -0400

    _redirects added

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<Map/>} />
-        <Route path="/" element={<Map/>} />
       </Routes>
     </>
 
